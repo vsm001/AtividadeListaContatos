2f18da7b54321e8e746bb263adb6593da0f61639
vsm001 <sanchez_victor10@hotmail.com>
2020-04-13 23:14:35 -0300

    ContatoAtualizar3

---
+++ style.js
@@ -0,0 +1,85 @@
+import {StyleSheet} from 'react-native';
+import Cores from './colors/colors';
+import Medidas from './measures/measures';
+
+export default StyleSheet.create({
+  container : {
+    flex: 1,
+        paddingHorizontal: Medidas.containerPaddingH,
+        marginTop: Medidas.containerMarginT,
+        backgroundColor: Cores.accent,
+  },
+  contatoView : {
+    padding: Medidas.cardPadding,
+    borderRadius: Medidas.cardBorderRadius,
+    backgroundColor: Cores.primary,
+    marginVertical: Medidas.cardMargin,
+    flexDirection: "column",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  contatoTextInput: {
+    width: Medidas.inputWidth,
+    height: Medidas.inputHeight,
+    backgroundColor: Cores.primary,
+    borderColor: Cores.border,
+    borderWidth: Medidas.inputBorderWidth,
+    borderRadius: Medidas.inputBorderRadius,
+    color: Cores.border,
+    //borderBottomColor: 'black',
+    //borderBottomWidth: 1,
+    marginBottom: Medidas.inputMargin,
+    padding: Medidas.inputPadding,
+
+  },
+  itemNaLista: {
+    padding: Medidas.cardPadding,
+    marginBottom: Medidas.cardMargin,
+    borderRadius: Medidas.cardBorderRadius,
+    backgroundColor: Cores.primary,
+    shadowColor: Cores.shadow,
+    shadowOffset: {
+      width: Medidas.cardShadowWidth,
+      height: Medidas.cardShadowHeight
+    },
+    shadowRadius: Medidas.cardShadowRadius,
+    shadowOpacity: Medidas.cardShadowOpacity,
+    elevation: Medidas.cardElevation,
+  },
+  listaTitle: {
+      fontSize: Medidas.fontText,
+      color: Cores.textPrimary,
+      fontWeight: 'bold',
+  },
+
+  listaPhone: {
+      marginTop: Medidas.cardTextMargin,
+      fontSize: Medidas.fontText,
+      color: Cores.textSecondary,
+  },
+  datailsButton: {
+    borderRadius: Medidas.buttonBorderRadius,
+    padding: Medidas.buttonPaddingPrimary,
+    paddingLeft: Medidas.buttonPaddingSecondary,
+    paddingRight: Medidas.buttonPaddingSecondary,
+    backgroundColor: Cores.accent,
+    alignItems: 'center'
+  },
+  detailsButtonText: {
+    color: Cores.primary,
+    fontWeight: 'bold',
+    fontSize: Medidas.fontButton,
+  },
+  welcome: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginTop: Medidas.titleMarginT,
+  },
+  welcomeTitle: {
+    fontSize: Medidas.fontTitle,
+    textAlign: 'center',
+    fontWeight: 'bold',
+    color: Cores.primary,
+  },
+
+});
